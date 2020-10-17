f015724bcb49bb4bcac5df00107d0b0d2b1c61e5
marktan2017 <mark.tan.2017@sis.smu.edu.sg>
2020-10-18 03:29:32 +0800

    Updates

---
+++ precache-manifest.9a5608b6f05d7370bbb9c99a59e3db94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "964dc7430c01655abe99f7d0d6331f9a",
+    "url": "/framer-motion-pizza-app/index.html"
+  },
+  {
+    "revision": "2bb6ba91af2d99f4c252",
+    "url": "/framer-motion-pizza-app/static/css/main.9ac09e15.chunk.css"
+  },
+  {
+    "revision": "d119b69eed7723e125e9",
+    "url": "/framer-motion-pizza-app/static/js/2.d7f06c03.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/framer-motion-pizza-app/static/js/2.d7f06c03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bb6ba91af2d99f4c252",
+    "url": "/framer-motion-pizza-app/static/js/main.1162b607.chunk.js"
+  },
+  {
+    "revision": "59f57cc63eedc5929762",
+    "url": "/framer-motion-pizza-app/static/js/runtime-main.cf565413.js"
+  }
+]);
